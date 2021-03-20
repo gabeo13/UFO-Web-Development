fba60d20ccf93c1513f9e8c1450f843469703d0b
Gabriel Allen <gabriel.houston.allen@gmail.com>
2021-03-20 17:18:09 -0600

    level1 filter complete

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,47 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Create event handlers 
+button.on("click", runFilter);
+form.on("submit", runFilter);
+
+// Complete the event handler function for the form
+function runFilter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Console log variables stepwise to ensure functionality
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Assign filted data to a variable
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    // Console log filtered data to ensure the filter worked
+    console.log(filteredData);
+
+    // Push filtered results to html table
+    filteredData.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
